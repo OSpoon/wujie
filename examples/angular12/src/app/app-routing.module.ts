4a2be998c0900bb812ebb824c13c4aec121f1f36
小鑫同学 <zxin088@gmail.com>
2024-04-02 10:40:02 +0800

    feat: 为angualr案例增加路由

--- examples/angular12/src/app/app-routing.module.ts
+++ examples/angular12/src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
+import { ContactComponent } from './pages/contact/contact.component';
+import { LocationComponent } from './pages/location/location.component';
+import { DialogComponent } from './pages/dialog/dialog.component';
+import { HomeComponent } from './pages/home/home.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [{ path: '**', children: [] }];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'dialog', component: DialogComponent },
+  { path: 'location', component: LocationComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }
--- examples/angular12/src/app/app.module.ts
+++ examples/angular12/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { DialogComponent } from './pages/dialog/dialog.component';
+import { LocationComponent } from './pages/location/location.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    DialogComponent,
+    LocationComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
